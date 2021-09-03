fee765f4bf103c707b8eee5f59a998884666ed03
NidalEllawala <arthurnidal@gmail.com>
2021-09-03 11:15:45 +0100

    feat server:install socket.io and initialize server

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,16 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Koa = require('koa');
+const app = new Koa();
 const router_1 = require("./router");
 const bodyParser = require('koa-bodyparser');
 const cors = require('@koa/cors');
+const httpServer = require('http').createServer(app.callback());
 const PORT = 3001;
-const app = new Koa();
 app.use(cors());
 app.use(bodyParser());
 app.use(router_1.router.routes());
 app.use(router_1.router.allowedMethods());
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
     console.log(`server listening on http://localhost:${PORT}`);
 });
+// app.listen(PORT, () => {
+//   console.log(`server listening on http://localhost:${PORT}`);
+// });
 //# sourceMappingURL=index.js.map
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,30 @@
 const Koa = require('koa');
+const app = new Koa();
 import { router } from './router';
 const bodyParser = require('koa-bodyparser');
 const cors = require('@koa/cors');
+import { Server, Socket } from "socket.io";
+const httpServer = require('http').createServer(app.callback());
+const io = new Server(httpServer);
+
+// ...
 
 
 const PORT = 3001;
 
-const app = new Koa();
 app.use(cors());
 app.use(bodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-app.listen(PORT, () => {
+io.on("connection", (socket: Socket) => {
+});
+
+httpServer.listen(PORT, () => {
   console.log(`server listening on http://localhost:${PORT}`);
 });
+
+
+// app.listen(PORT, () => {
+//   console.log(`server listening on http://localhost:${PORT}`);
+// });
